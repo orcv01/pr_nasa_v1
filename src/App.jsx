4814e4527ca25632c9b4e3fbebb17f644781dce4
UsuarioM <orcv01@gmail.com>
2023-10-05 13:56:25 +0200

    pr_nasa_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+// Axccont Id 888608be-4c46-4c5b-8362-a12adc8d51cd
+// Api Key JH1tOnIagCfaBo6CBWvIrVPw0JHTu6omcXqeHkly
 
-function App() {
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Axios } from 'axios';
+import Figure from './components/Figure/Figure';
+
+
+const App = () => {
+  const today = new Date(Date.now()).toISOString().slice(0, 10);
+  const [apod, setApod] = useState({});
+  const [date, setDate] = useState(today);
+  
+  const NASA_URL = "https://api.nasa.gov/";
+  const NASA_API_KEY = "JH1tOnIagCfaBo6CBWvIrVPw0JHTu6omcXqeHkly";
+  
+   useEffect(() => {
+    const getApod = async () => {
+    const data = await axios.get(
+    `${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`
+    );
+    setApod(data.data);
+    };
+    getApod();
+  }, [date]);
 
   return (
-    <>
-    </>
+    <div className="App">
+      <h1 className="title">
+        NASA API <img src={'https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg'} className="logo" alt="NASA LOGO" />
+      </h1>
+      <h2>Astronomy Picture of the Day</h2>
+        <input type="date" id="photo-date" onChange={handleInput} />
+        {date > today ? (<h2>Please choose a previous date</h2>) 
+        : (<Figure data={apod} />)}
+      <div className="standard-dialog center">
+        <h2 className="dialog-text">@lethamburn - 2022 - <a href="https://api.nasa.gov/">https://api.nasa.gov/</a></h2>
+      </div>
+  </div>
   )
-}
+};
 
-export default App
+export default App;
+
---
+++ src/components/Figure/Figure.jsx
@@ -0,0 +1,12 @@
+import "./Figure.css";
+
+const Figure = () => {
+
+    return (
+        <figure>
+            <h2>Photo Diary</h2>
+        </figure>
+    )
+}
+
+export default Figure;
